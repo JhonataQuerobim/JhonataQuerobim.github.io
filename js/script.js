caf918674d749cda6af8d02fad1c9584237de6d8
JhonataQuerobim <jhonataquerobim@gmail.com>
2019-10-24 12:10:35 -0300

    Atualizando as capitais 3

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $.ajax({
     //url: "https://api.myjson.com/bins/gbr4g",
     //url: "https://api.myjson.com/bins/ijsao",
     //url: "https://api.myjson.com/bins/qoydw",
-    url: "https://api.myjson.com/bins/71b2s",
+    url: "https://api.myjson.com/bins/116hfo",
     dataType: "text",
     mimeType: "textPlain",
     async: false,
